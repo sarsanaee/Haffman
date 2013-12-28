51cd84166ba3603f98c5b55616dd54eee9062606
Alireza Sanaee <sarsanaee@gmail.com>
2013-12-29 00:07:36 +0330

    writing decode function

--- haffman.py
+++ haffman.py
@@ -4,7 +4,7 @@ from sys import argv
 from Node import Node
 
 class Hafman():
-    """khodet ye niga bendaz bebin in tree maker ro mitooni doros koni ya na"""
+    """This function will do all the progerss you must trace this class to figure out """
 
     def __init__(self, MyFile):
         self.File = str(open(MyFile, 'r').read())
@@ -16,7 +16,7 @@ class Hafman():
         self.currentNode = None
 
     def repeatFinder(self):
-        """ in tabe kheili rahate niazi be tozih nadare"""
+        """ making self.Data for finding the number that each char repeated"""
         for i in self.File:
             if i in self.Data:
                 self.Data[i] += 1
@@ -28,19 +28,16 @@ class Hafman():
         self.dataQueue.sort(key=lambda node: node.getValue()[0])
 
     def treeMaker(self):
-        """in tabe derakhte hafman roo ba komake tavabe class haye digar doros mikone"""
+        """As it shows on his face this function makes huffman tree"""
 
         while len(self.dataQueue) > 1:
-            temp1, temp2 = self.dataQueue.pop(0), self.dataQueue.pop(0)  # kharej kardan do onsor az safe olaviat
-            #print(temp1.getValue(), temp2.getValue())   # baraye moshahedeye ettefaghat
-            self.dataQueue.append(self.nodeJoin(temp1, temp2))  # vared kardan node jadide ijad shode be safe olaviat
+            temp1, temp2 = self.dataQueue.pop(0), self.dataQueue.pop(0)  
+            self.dataQueue.append(self.nodeJoin(temp1, temp2))  
             self.dataQueue.sort(key=lambda node: node.getValue()[0])
-            print(self.dataQueue)
-                # ta in khat bayad derakht ma doros beshe hala dorosto ghalatesho khodet ye niga benadaz
         return self.currentNode
 
-    ###################################
     def nodeJoin(self, right, left):
+        '''when we pop two object from queue then we merge them by this function and make new Node'''
         if right.getValue()[0] > left.getValue()[0]:
             l = left
             r = right
@@ -52,8 +49,8 @@ class Hafman():
         self.currentNode.setRightChild(r)
         return self.currentNode
 
-    #######################################################
     def encode(self):
+        '''encoding our file'''
         self.map = {}
         tree = self.treeMaker()
         for i in self.Data.keys():
@@ -72,10 +69,31 @@ class Hafman():
         coded_file.close()
         for i in self.File:
             self.coded += self.map[i]
-        with open(self.file_name + '.bin', 'wb') as f:                     # write the encoded data to a binary file
+        with open(self.file_name + '.bin', 'wb') as f:
             b = BitArray(bin=self.coded)
             f.write(b.tobytes())
         #####
+
+    @staticmethod
+    def decode(binary_file,map_file):
+        '''decoding our file'''
+        mapping_dict = eval(open(map_file,"r").read())
+        newfile = open('new_decoded_File.txt', 'a')
+        tem_map = {}
+        for i in range(len(mapping_dict)):
+            b = mapping_dict.popitem()
+            tem_map[b[1]] = b[0]
+        reading_file = open(binary_file,"rb")
+        reading_binary = str(BitStream(reading_file.read()).bin)
+        code = ''
+        for i in reading_binary:
+            code = code + i
+            if code in tem_map:
+                newfile.write(tem_map[code])
+                code = '' 
+        newfile.close()
+        reading_file.close()
+
 
 
 def main():
@@ -85,11 +103,13 @@ def main():
         a.repeatFinder()
         a.encode()
 
-    #elif mode == 'd':    # this is for you to complete   argv[1] = mode     argv[2] = binary file    argv[3] = map file
-
+    elif mode == 'd':    # this is for you to complete   argv[1] = mode     argv[2] = binary file    argv[3] = map file
+        Hafman.decode(argv[2],argv[3])
 #main()
-a = Hafman('MyFile.txt')
-a.repeatFinder()
-a.encode()
+#a = Hafman('MyFile.txt')
+#a.repeatFinder()
+#a.encode()
+#Hafman.decode('MyFile.bin','MyFile_map.txt')
 
+main()
 
